d24d07381e8badd93654dec2453b8ea1e18b63fe
Ana St.Aubyn <anacstaubyn@gmail.com>
2025-08-11 19:36:00 +0100

    chapter 2: variables

---
+++ ruby_basics/2_variables/variables.rb
@@ -0,0 +1,25 @@
+# Declaring a variable 
+my_variable = "Hello, World!"
+age = 18
+age = 18 + 5
+# Printing the variable
+puts my_variable
+puts age
+
+# Reassigning a variable
+age += 4
+puts "Age after 4 years:"
+puts age
+age -= 2
+puts "Age after 2 years:"
+puts age
+
+cash = 10
+cash *= 2
+puts "Cash after doubling:"
+puts cash
+
+temperature = 30
+temperature /= 10
+puts "Temperature after division by 10:"
+puts temperature
